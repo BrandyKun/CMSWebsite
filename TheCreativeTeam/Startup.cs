8c8c15de0780181d80831b1ea9e91eea2543ac06
brandy <machumbi.kundenga@iris.co.uk>
2021-01-04 11:00:39 +0000

    Add project files.

---
+++ TheCreativeTeam/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Modules;
+using OrchardCore.ResourceManagement;
+
+namespace TheCreativeTeam
+{
+    public class Startup : StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            //rviceCollection.AddScoped<IResourceManifestProvider, ResourceManifest>();
+        }
+    }
+}
